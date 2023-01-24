ae465b1386edeb8cbdaaf0fa5843fe6b100b6aa1
amannitp <amannitp272@gmail.com>
2023-01-24 11:47:53 +0530

    functional component used in react

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
 
 import './App.css';
+import Header from './Header';
 
 function App() {
-  let name="Aman Raj"
-  let rollNo=1902036
+  const name="Aman Raj"
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>{name}</h1>
-        <h1>Roll no :{rollNo}</h1>
-        <p>
-          React js full course
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Header/>
+        <h2>  My  name is {name}</h2>
     </div>
   );
 }
---
+++ src/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <h1>This is  the header section</h1>
+  )
+}
+
+export default Header
