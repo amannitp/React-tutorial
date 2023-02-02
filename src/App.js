42afb6ebccd1113d0e7bad478fcdefcdcb787ebc
amannitp <amannitp272@gmail.com>
2023-02-02 13:28:31 +0530

    use of useEffect in 3 diffrent way in react

--- src/App.js
+++ src/App.js
@@ -13,68 +13,96 @@ import AddIcon from '@mui/icons-material/Add';
 import { useState } from 'react';
 import DataField from './Component/DataField';
 import DeleteIcon from '@mui/icons-material/Delete';
-
+import UseEffectTut from './Component/UseEffectTut';
+import { useEffect } from 'react';
 
 function App() {
 
-  const [name,setName]=useState("")
-  const [email,setEmail]=useState("")
-  const [data,setData]=useState([])
+  const[count,setCount]=useState(0)
 
-  const addData = ()=>{
-    setData([...data,{
-      name:name,
-      email:email
-    }]);
-    setName("")
-    setEmail("")
-  }
+  // useEffect(()=>{
+  //   alert("alert ")
+  // })
+
+  // useEffect(()=>{
+  //   console.log("helllo world ")
+  // },[])
+
+  useEffect(()=>{
+    console.log("hello world")
+  },[count])
+
+
+  return(
+    <div>
+      {console.log(" re-render")}
+    <UseEffectTut/>
+    <button onClick={()=>setCount(count+1)}>click Me</button>
+    </div>
+  )
+
+    
+
+
+
+  // const [name,setName]=useState("")
+  // const [email,setEmail]=useState("")
+  // const [data,setData]=useState([])
+
+  // const addData = ()=>{
+  //   setData([...data,{
+  //     name:name,
+  //     email:email
+  //   }]);
+  //   setName("")
+  //   setEmail("")
+  // }
   
-  return (
-    <div className="app">
-        <Header/>
-        {/* form */}
-        <div className='form'>
-        <Stack direction="row" spacing={2}>
-        <TextField
-        value={name}
-        onChange={(event)=>setName(event.target.value)}
-         id="outlined-basic " 
-         className='tf1' 
-         label="name" 
-         variant="outlined" />
-        <TextField 
-        value={email}
-        onChange={(event)=>setEmail(event.target.value)}
-        id="outlined-basic" 
-        className='tf2' 
-        label="email" 
-        variant="outlined" />
-        <Button onClick={addData} variant="contained" color="success">
-             <AddIcon/>
-        </Button>
-        </Stack>
+  // return (
+  //   <div className="app">
+  //       <Header/>
+  //       {/* form */}
+  //       <div className='form'>
+  //       <Stack direction="row" spacing={2}>
+  //       <TextField
+  //       value={name}
+  //       onChange={(event)=>setName(event.target.value)}
+  //        id="outlined-basic " 
+  //        className='tf1' 
+  //        label="name" 
+  //        variant="outlined" />
+  //       <TextField 
+  //       value={email}
+  //       onChange={(event)=>setEmail(event.target.value)}
+  //       id="outlined-basic" 
+  //       className='tf2' 
+  //       label="email" 
+  //       variant="outlined" />
+  //       <Button onClick={addData} variant="contained" color="success">
+  //            <AddIcon/>
+  //       </Button>
+  //       </Stack>
         
-        </div>
+  //       </div>
 
-        {/* data */}
+  //       {/* data */}
 
-        <div className='data'>
-          <div className='data_val'>
-          <h4>Name</h4>
-          <h4>Email</h4>
-           <h4>Remove</h4>
+  //       <div className='data'>
+  //         <div className='data_val'>
+  //         <h4>Name</h4>
+  //         <h4>Email</h4>
+  //          <h4>Remove</h4>
 
-          </div>
-          {
-            data.map((element,idx)=>{
-              return(
-                <DataField key={idx} name={element.name} email={element.email} idx={idx}/>
-              )
-            })
-          }
+  //         </div>
+  //         {
+  //           data.map((element,idx)=>{
+  //             return(
+  //               <DataField key={idx} name={element.name} email={element.email} idx={idx}/>
+  //             )
+  //           })
+  //         }
 
-        </div>
+  //       </div>
 
 
 
@@ -140,8 +168,7 @@ function App() {
         {/* <Header/>
         <h2 style={{backgroundColor:"red",color:'black'}}>  My  name is {name}</h2>
         <Footer/> */}
-    </div>
-  )
+   
 }
 
 export default App;
---
+++ src/Component/UseEffectTut.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function UseEffectTut() {
+  return (
+    <h1>useEffect tutorial </h1>
+  )
+}
+
+export default UseEffectTut
