1a96eaa703b4657e54e847aac3f362586b6e22e5
amannitp <amannitp272@gmail.com>
2023-02-03 10:38:24 +0530

    study some other event which frequently used in react

--- src/Component/EventTut.jsx
+++ src/Component/EventTut.jsx
@@ -1,20 +1,37 @@
 import React from 'react'
 import TextField from '@mui/material/TextField';
 import '../App.css'
+import Button from '@mui/material/Button';
 
 const EventTut = () => {
 
     const changeOccurance= () =>{
          window.alert(" donot copy")
     }
+
+    const paste=()=>{
+        window.alert( " donot paste the word ")
+    }
+
+    const clickevent=()=>{
+        window.alert(" double click event is occurs ")
+    }
   return (
     <div className='Event-Tut'>
         <TextField
+        style={{margin:'10px'}}
         onCopy={changeOccurance}
+        onPaste={paste}
          id="outlined-basic" 
          label="name" 
          variant="outlined" />
-        <TextField id="outlined-basic" label="email" variant="outlined" />
+        <TextField
+        style={{margin:'10px'}}
+         id="outlined-basic" label="email" variant="outlined" />
+        <Button
+        style={{margin:'10px'}}
+        onDoubleClick={clickevent}
+         color="error">Secondary</Button>
     </div>
   )
 }
