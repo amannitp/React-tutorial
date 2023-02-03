de5b0b2e057203afb233391c262c3893325e45d2
amannitp <amannitp272@gmail.com>
2023-02-03 13:03:19 +0530

    study about react router dom

--- src/App.js
+++ src/App.js
@@ -16,6 +16,19 @@ import DeleteIcon from '@mui/icons-material/Delete';
 import UseEffectTut from './Component/UseEffectTut';
 import { useEffect } from 'react';
 import EventTut from './Component/EventTut';
+import Contact from './Component/Contact';
+import Home from './Component/Home';
+import About from './Component/About';
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+  useRouteMatch,
+  useParams
+} from "react-router-dom";
+import Error from './Component/Error';
 
 function App() {
 
@@ -48,153 +61,19 @@ function App() {
 
 
   return(
-
-    <EventTut/>
-    // <div style={{backgroundColor:"red"}}>
-    // <UseEffectTut/>
-    // <button onClick={()=>setCount(count+1)}>click Me</button>
-    //  {
-    //   data.map((element,idx)=>{
-    //     return(
-    //       <div key={idx}>
-    //         <h1>{element.firstName}</h1>
-    //         <h1>{element.lastName}</h1>
-    //       </div>
-
-    //     )
-    //   })
-    //  }
-    // </div>
-  )
+    <Router>
+    <Header/>
+    
+      <Routes>
+      <Route path='/Home' element={<Home/>}/>
+      <Route path='/contact' element={<Contact/>}/>
+      <Route path='/about' element={<About/>}/>
+      <Route path='*' element={<Error/>}/>
+      </Routes>
+    </Router>
 
     
-
-
-
-  // const [name,setName]=useState("")
-  // const [email,setEmail]=useState("")
-  // const [data,setData]=useState([])
-
-  // const addData = ()=>{
-  //   setData([...data,{
-  //     name:name,
-  //     email:email
-  //   }]);
-  //   setName("")
-  //   setEmail("")
-  // }
-  
-  // return (
-  //   <div className="app">
-  //       <Header/>
-  //       {/* form */}
-  //       <div className='form'>
-  //       <Stack direction="row" spacing={2}>
-  //       <TextField
-  //       value={name}
-  //       onChange={(event)=>setName(event.target.value)}
-  //        id="outlined-basic " 
-  //        className='tf1' 
-  //        label="name" 
-  //        variant="outlined" />
-  //       <TextField 
-  //       value={email}
-  //       onChange={(event)=>setEmail(event.target.value)}
-  //       id="outlined-basic" 
-  //       className='tf2' 
-  //       label="email" 
-  //       variant="outlined" />
-  //       <Button onClick={addData} variant="contained" color="success">
-  //            <AddIcon/>
-  //       </Button>
-  //       </Stack>
-        
-  //       </div>
-
-  //       {/* data */}
-
-  //       <div className='data'>
-  //         <div className='data_val'>
-  //         <h4>Name</h4>
-  //         <h4>Email</h4>
-  //          <h4>Remove</h4>
-
-  //         </div>
-  //         {
-  //           data.map((element,idx)=>{
-  //             return(
-  //               <DataField key={idx} name={element.name} email={element.email} idx={idx}/>
-  //             )
-  //           })
-  //         }
-
-  //       </div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        {/* <UseStateHooks/> */}
-
-    
-    {/* if else condition used in react
-      {
-        (()=>{
-          if(login){
-            return <h1>Hello i am Aman Raj</h1>
-          }else{
-            return <h1> i am kamlesh kumar</h1>
-          }
-        })()
-      } */}
-
-     {/* {
-      login===true ?<Header/>:<Footer/>
-     } */}
-
-      {/* {/* {
-          movies.map((ele)=>{
-
-          return(
-            <Movie/>
-          )
-          }
-      } */}
-
-     
-      {/* <div className='main'>
-      <Moviedata/> */}
-
-
-
-
-      
-      {/* <StudentProps 
-      name="Aman Raj"
-       rollNo="1902036"
-       />
-      <StudentProps 
-      name="Ajay Kumer sharma" 
-      rollNo="1902020"
-      />
-      <StudentProps 
-      name="vivek kumar"
-       rollNo="1902021"
-       /> */}
-
-        {/* <Header/>
-        <h2 style={{backgroundColor:"red",color:'black'}}>  My  name is {name}</h2>
-        <Footer/> */}
-   
-}
+    )
+  }
 
 export default App;
--- src/Component/Header.jsx
+++ src/Component/Header.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import '../Component/footer.css'
 import '../Component/Header.css'
+import { Link } from 'react-router-dom'
 function Header() {
   return (
     <div className='header-section'>
-        <h1 className='header'>Record keeping</h1>
+      <div className='header'>
+        <Link to={'./home'}><h1 style={{cursor:'pointer'}}>Home</h1></Link>
+        <Link to='./about'><h1 style={{cursor:'pointer'}}>About</h1></Link>
+        <Link to={'./contact'}><h1 style={{cursor:'pointer'}} >Contact</h1></Link>
+        </div>
+        
     </div>
   )
 }
